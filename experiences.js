81282b50be7bb66fe4511cec4588a87273f69d3b
Aaron Aquino <Aaron@Aarons-MacBook-Pro-3.local>
2016-09-17 23:17:12 -0700

    Removed references to home.html

--- experiences.js
+++ experiences.js
@@ -4,18 +4,18 @@
 document.getElementById("copyright-footer").innerHTML =
 	"<p class='copyright-text'>&copy;  " +
 	new Date().getFullYear() +
-	" Copyright <a class='link-no-underline w3-hover-text-strawberry-red' href='../home.html'>Aaron Aquino</a>. All rights reserved.";
+	" Copyright <a class='link-no-underline w3-hover-text-strawberry-red' href='../index.html'>Aaron Aquino</a>. All rights reserved.";
 
 
 /**
  * Adds the navigation buttons.
  */
 document.getElementById("menu").innerHTML = "<ul class='w3-navbar w3-large w3-padding-8 w3-white w3-container'>" +
-	"<a class='w3-hover-white' href='../home.html'><img src='../images/ninja.svg' alt='Logo' id='logo' class='w3-small'></a>" + 
+	"<a class='w3-hover-white' href='../index.html'><img src='../images/ninja.svg' alt='Logo' id='logo' class='w3-small'></a>" + 
 	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='../dancer.html'>DANCER</a></li>" +
 	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='../hacker.html'>HACKER</a></li>" +
 	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='../connector.html'>CONNECTOR</a></li>" +
-	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='../home.html'>HOME</a></li>" +
+	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='../index.html'>HOME</a></li>" +
 	"</ul>";
 
 
