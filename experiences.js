c745b6f5490f80fe58faa76d0277e29e5a2c422d
Aaron Aquino <Aaron@DN51vcpt.SUNet>
2016-11-03 01:19:24 -0700

    Fixed social media icon bug.

--- experiences.js
+++ experiences.js
@@ -18,16 +18,4 @@ document.getElementById("menu").innerHTML = "<ul class='w3-navbar w3-large w3-pa
 	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='../index.html'>HOME</a></li>" +
 	"</ul>";
 
-
-/**
- * Adds the social media icons. The spaces after each </a> tag are necessary to create space between the icons.
- */
-var socialIcons = document.getElementById("social-icons");
-if (socialIcons) {
-	socialIcons.innerHTML = 
-"<a href='https://www.facebook.com/aaquino33' id='facebookLink' class='icon-3x icon-facebook-sign link-no-underline w3-hover-text-strawberry-red' aria-label='Facebook'></a> " +
-"<a href='https://www.linkedin.com/in/aaronaquino33' id='linkedinLink' class='icon-3x icon-linkedin-sign link-no-underline w3-hover-text-strawberry-red' aria-label='LinkedIn'></a> " +
-"<a href='https://github.com/aaronaquino' id='githubLink' class='icon-3x icon-github-sign link-no-underline w3-hover-text-strawberry-red' aria-label='Github'></a> " +
-"<a href='https://www.instagram.com/aaronaquino33/' id='instagramLink' class='icon-3x icon-instagram-sign link-no-underline w3-hover-text-strawberry-red' aria-label='Instagram'></a>";
-}
  
