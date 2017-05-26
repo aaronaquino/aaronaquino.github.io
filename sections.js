cd6b0369b2e3d27accfb825033e53695dac492ed
Aaron Aquino <aaron33@stanford.edu>
2017-05-26 12:53:26 +0200

    Configure the viewport to support mobile-first.

--- sections.js
+++ sections.js
@@ -11,11 +11,11 @@ document.getElementById("copyright-footer").innerHTML =
  * Adds the navigation menu and buttons.
  */
 document.getElementById("menu").innerHTML = "<ul class='w3-navbar w3-large w3-padding-8 w3-white w3-container'>" +
-	"<a class='w3-hover-white' href='index.html'><img src='images/logo.png' alt='Logo' id='logo' class='w3-small'></a>" + 
-	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='dancer.html'>DANCER</a></li>" +
-	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='hacker.html'>HACKER</a></li>" +
-	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='connector.html'>CONNECTOR</a></li>" +
+	"<a class='w3-hover-white' href='index.html'><img src='images/logo.png' alt='Logo' id='logo' class='w3-small'></a>" +
 	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='index.html'>HOME</a></li>" +
+	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='dancer.html'>DANCER</a></li>" +
+	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='connector.html'>CONNECTOR</a></li>" +
+	"<li class='w3-right'><a class='w3-text-carbon w3-hover-white w3-hover-text-strawberry-red' href='hacker.html'>HACKER</a></li>" +
 	"</ul>";
 
 
