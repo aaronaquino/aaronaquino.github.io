92d654d09e347d18d940cecbc07dc7ebcb19a7d6
Aaron Aquino <aaron33@stanford.edu>
2018-06-27 17:44:20 -0700

    Added Spotify to social media links

--- homepage.js
+++ homepage.js
@@ -15,8 +15,9 @@ $(document).ready(function (){
 var socialIcons = document.getElementById("social-icons");
 if (socialIcons) {
 	socialIcons.innerHTML = 
-"<a href='https://www.linkedin.com/in/aaronaquino33' id='linkedinLink' class='icon-3x icon-linkedin-sign link-no-underline w3-hover-text-theme-dark' aria-label='LinkedIn'></a> " +
-"<a href='https://www.instagram.com/aaronaquino33/' id='instagramLink' class='icon-3x icon-instagram-sign link-no-underline w3-hover-text-theme-dark' aria-label='Instagram'></a> " +
-"<a href='https://www.youtube.com/channel/UCbnjLftIE7Xs_68Ktm4vonQ' id='youtubeLink' class='icon-3x icon-youtube-sign link-no-underline w3-hover-text-theme-dark' aria-label='YouTube'></a>";
+"<a href='https://www.linkedin.com/in/aaronaquino33' target='_blank' class='icon-3x icon-linkedin-sign link-no-underline w3-hover-text-theme-dark' aria-label='LinkedIn'></a> " +
+"<a href='https://www.instagram.com/aaronaquino33' target='_blank' class='icon-3x icon-instagram-sign link-no-underline w3-hover-text-theme-dark' aria-label='Instagram'></a> " +
+"<a href='https://open.spotify.com/user/123165466' target='_blank' class='icon-3x icon-spotify-sign link-no-underline w3-hover-text-theme-dark' aria-label='Spotify'></a> " +
+"<a href='https://www.youtube.com/channel/UCbnjLftIE7Xs_68Ktm4vonQ' target='_blank' class='icon-3x icon-youtube-sign link-no-underline w3-hover-text-theme-dark' aria-label='YouTube'></a>";
 }
  
