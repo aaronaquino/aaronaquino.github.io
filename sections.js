fa18c88196dbd32a9de96be09dd2bffb64cb027c
Aaron Aquino <aaron33@stanford.edu>
2018-07-08 19:39:41 -0700

    Added Creative section

--- sections.js
+++ sections.js
@@ -13,6 +13,7 @@ document.getElementById("copyright-footer").innerHTML =
 document.getElementById("menu").innerHTML = "<ul class='w3-navbar w3-large w3-padding-8 w3-container'>" +
 	"<a href='index.html'><img src='assets/logo_black.png' alt='Logo' id='logo' class='w3-small'></a>" +
 	"<li class='w3-right'><a class='w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='index.html'>HOME</a></li>" +
+	"<li class='w3-right'><a class='w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='creative.html'>CREATIVE</a></li>" +
 	"<li class='w3-right'><a class='w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='dancer.html'>DANCER</a></li>" +
 	"<li class='w3-right'><a class='w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='connector.html'>CONNECTOR</a></li>" +
 	"<li class='w3-right'><a class='w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='hacker.html'>HACKER</a></li>" +
