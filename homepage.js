c74fc95ff7df1a3964aabce89606022470cb74c8
Aaron Aquino <aaron33@stanford.edu>
2018-07-24 18:51:36 -0700

    Updated YouTube channel link

--- homepage.js
+++ homepage.js
@@ -18,6 +18,6 @@ if (socialIcons) {
 "<a href='https://www.linkedin.com/in/aaronaquino33' target='_blank' class='icon-3x icon-linkedin-sign link-no-underline w3-hover-text-theme-dark' aria-label='LinkedIn'></a> " +
 "<a href='https://www.instagram.com/aaronaquino33' target='_blank' class='icon-3x icon-instagram-sign link-no-underline w3-hover-text-theme-dark' aria-label='Instagram'></a> " +
 "<a href='https://open.spotify.com/user/123165466' target='_blank' class='icon-3x icon-spotify-sign link-no-underline w3-hover-text-theme-dark' aria-label='Spotify'></a> " +
-"<a href='https://www.youtube.com/channel/UCbnjLftIE7Xs_68Ktm4vonQ' target='_blank' class='icon-3x icon-youtube-sign link-no-underline w3-hover-text-theme-dark' aria-label='YouTube'></a>";
+"<a href='https://www.youtube.com/c/aaronaquino' target='_blank' class='icon-3x icon-youtube-sign link-no-underline w3-hover-text-theme-dark' aria-label='YouTube'></a>";
 }
  
