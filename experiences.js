5da6e511824bb41365cad9a188ee4c93a9fdb84a
Aaron Aquino <aaron33@stanford.edu>
2018-08-11 11:13:04 -0700

    Downsized logo for faster loading

--- experiences.js
+++ experiences.js
@@ -11,7 +11,7 @@ document.getElementById("copyright-footer").innerHTML =
  * Adds the navigation menu and buttons.
  */
 document.getElementById("menu").innerHTML = "<div id='myTopNav' class='w3-large w3-padding-8 topnav'>" +
-	"<a href='../index.html' class='mobile-nav'><img src='../assets/logo_black.png' alt='Logo' id='logo' class='w3-small'></a>" +
+	"<a href='../index.html' class='mobile-nav'><img src='../assets/logo.png' alt='Logo' id='logo' class='w3-small'></a>" +
 	"<a href='javascript:void(0);' id='hamburgerButton' class='w3-right w3-text-carbon w3-hover-text-theme-dark hamburger-icon mobile-nav' onclick='toggleTopNav()'><i class='fa fa-bars'></i></a>" +
 	"<a class='w3-right w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='../index.html'>ABOUT</a>" +
 	"<a class='w3-right w3-hover-white w3-text-carbon w3-hover-text-theme-dark' href='../creative.html'>CREATIVE</a>" +
