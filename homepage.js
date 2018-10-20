2b82dd0e24b12ac67b7185b9bdf56172d034040e
Aaron Aquino <aaron33@stanford.edu>
2018-10-19 17:42:10 -0700

    Down arrow no longer shows focus for mouse clicks

--- homepage.js
+++ homepage.js
@@ -1,4 +1,17 @@
 /**
+ * Prevents the down arrow from showing visible focus unless the user is navigating with the tab key.
+ * Tip taken from: https://hackernoon.com/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2.
+ */
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // The "I am a keyboard user" key.
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
+/**
  * Scrolls down to the bio when the down arrow is clicked.
  */
 $(document).ready(function (){
