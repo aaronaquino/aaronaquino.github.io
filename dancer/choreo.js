18013dc105d2413040a9f839eea6bf3f883231ff
Aaron Aquino <aaron33@stanford.edu>
2020-06-24 22:13:51 -0700

    Remove old dance videos.

--- dancer/choreo.js
+++ dancer/choreo.js
@@ -14,22 +14,10 @@ $(document).ready(function () {
 	$('#liftoff-iframe').on('load', function () {
 		$('#liftoff-spinner').hide();
 	});
-	$('#21-iframe').on('load', function () {
-		$('#21-spinner').hide();
-	});
 	$('#fall-iframe').on('load', function () {
 		$('#fall-spinner').hide();
 	});
-	$('#closer-iframe').on('load', function () {
-		$('#closer-spinner').hide();
-	});
 	$('#baby-iframe').on('load', function () {
 		$('#baby-spinner').hide();
-	});
-	$('#photograph-iframe').on('load', function () {
-		$('#photograph-spinner').hide();
-	});
-	$('#fym-iframe').on('load', function () {
-		$('#fym-spinner').hide();
 	});
 });
