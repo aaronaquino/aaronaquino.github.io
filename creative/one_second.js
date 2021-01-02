f5309a75eee21cc8bdb04d765e0c6b027167c865
Aaron Aquino <aaron33@stanford.edu>
2021-01-01 16:35:04 -0800

    Update 1SE page with 2020 video

--- creative/one_second.js
+++ creative/one_second.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-	$('#one-second-iframe').on('load', function () {
-		$('#one-second-spinner').hide();
+	$('#one-second-2020-iframe').on('load', function () {
+		$('#one-second-2020-spinner').hide();
+	});
+	$('#one-second-2019-iframe').on('load', function () {
+		$('#one-second-2019-spinner').hide();
+	});
+	$('#one-second-2018-iframe').on('load', function () {
+		$('#one-second-2018-spinner').hide();
+	});
+	$('#one-second-2017-iframe').on('load', function () {
+		$('#one-second-2017-spinner').hide();
 	});
 });
