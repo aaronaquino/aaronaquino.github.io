0590ae0035ee04bd20fdeebef5697d80bd77e8f9
Aaron Aquino <aaron@Aarons-MacBook-Pro.local>
2023-01-01 23:02:32 -0800

    Fix config site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'aaronaquino.co',
+    site: 'https://aaronaquino.co',
     integrations: [react()],
 });
